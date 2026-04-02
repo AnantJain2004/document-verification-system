1fa2a6045bd6c3e550ee46fd903c303c92e10c9c
Anant Jain <ajanantzen2004@gmail.com>
2026-04-02 19:50:58 +0530

    Remove window specific paths

--- utils/ocr.py
+++ utils/ocr.py
@@ -2,15 +2,16 @@ from pdf2image import convert_from_path
 import pytesseract
 
 # Tesseract path
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 def extract_text(pdf_path):
-    images = convert_from_path(
-        pdf_path,
-        poppler_path=r"C:\poppler\poppler-25.12.0\Library\bin"
-    )
-
+    # images = convert_from_path(
+    #     pdf_path,
+    #     poppler_path=r"C:\poppler\poppler-25.12.0\Library\bin"
+    # )
+    images = convert_from_path(pdf_path)
     text = ""
+    
     for img in images:
         text += pytesseract.image_to_string(img)
 
