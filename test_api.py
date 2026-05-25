8229f73ec4ea227efa05c391258d0629f3507311
Anant Jain <ajanantzen2004@gmail.com>
2026-05-26 02:38:45 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,9 +1,14 @@
 import requests
+from utils.ocr import extract_text
 
+file_path = "test.pdf"
+text = extract_text(file_path)
+
+print(text[:500])
 url = "https://doc-verifier.onrender.com/predict"
 
 files = {"file": open("temp/test.pdf", "rb")}
 
 response = requests.post(url, files=files)
 
-print(response.json())
+print("JSON:",response.json())
